22e2acea6542e907130fca074ee492c3573b2916
Travis Schall <schallt2015@gmail.com>
2020-05-29 18:45:06 -0400

    Updated version of the game
    Updated the game with options for custom parameters and more logic checks

--- GuessingGame.py
+++ GuessingGame.py
@@ -11,22 +11,19 @@ class GuessNumber:
         self.num_guesses = 0
 
         # Constants for difficulty settings
-        self.EASY_DIFFICULTY = 10
-        self.MEDIUM_DIFFICULTY = 50
-        self.HARD_DIFFICULTY = 100
+        self.SETTINGS = ('easy', 'medium', 'hard', 'custom')
+        self.MAX_SETTINGS = {'easy': 10, 'medium': 50, 'hard': 100}
 
     # Sets the difficulty for the game between easy, medium, and hard
     def set_difficulty(self):
-        pick = input(f'What difficulty would you like to play at? (Easy, Medium, Hard)\n').lower()
+        pick = input(f'What difficulty would you like to play at? (Easy, Medium, Hard, Custom)\n').lower()
 
         # Uses player input to set difficulty based on constants already set
         if self.check_difficulty(pick):
-            if pick == 'easy':
-                self.max = self.EASY_DIFFICULTY
-            elif pick == 'medium':
-                self.max = self.MEDIUM_DIFFICULTY
-            elif pick == 'hard':
-                self.max = self.HARD_DIFFICULTY
+            if pick == 'custom':
+                self.custom_difficulty()
+            else:
+                self.max = self.MAX_SETTINGS[pick]
         else:
             print('Invalid input')
             self.set_difficulty()
@@ -34,27 +31,59 @@ class GuessNumber:
         # Generates the random number inside of set parameters
         self.goal = rand(self.min, self.max)
 
-    # Checks the word if it is correct or not
+    # Set custom difficulty parameters
+    def custom_difficulty(self):
+        print('Enter a custom minimum and maximum value for the game.')
+        tMin = input('Minimum: ')
+        tMax = input('Maximum: ')
+
+        if self.check_custom(tMin, tMax):
+            self.min = int(tMin)
+            self.max = int(tMax)
+        else:
+            print('Invalid minimum and maximum inputs')
+            self.custom_difficulty()
+
+        # Takes in a guess from the user and calls the logic check for the number
+    def get_guess(self):
+        guess = input(f'Make a guess between {self.min} and {self.max} ')
+
+        if self.check_guess(guess):
+            return int(guess)
+        else:
+            print(f'Enter a valid number.')
+            return self.get_guess()
+
+    # Checks user difficulty input against preset settings
     def check_difficulty(self, pick):
         try:
             word = str(pick)
         except:
             return False
 
-        return pick == 'easy' or pick == 'medium' or pick == 'hard'
+        for check in self.SETTINGS:
+            if word == check:
+                return True
 
-    # Takes in a guess from the user and calls the logic check for the number
-    def get_guess(self):
-        guess = input(f'Make a guess between {self.min} and {self.max} ')
+        return False
 
-        if self.check_number(guess):
-            return int(guess)
-        else:
-            print(f'Enter a valid number.')
-            return self.get_guess()
+    # Logic check for custom difficulty setting parameters submitted by user
+    def check_custom(self, test_min, test_max):
+        try:
+            tMax = int(test_max)
+            tMin = int(test_min)
+        except:
+            return False
+
+        if tMin < 0:
+            return False
+        elif tMin > tMax:
+            return False
+
+        return True
 
     # Logic check for the user's guesses
-    def check_number(self, guess):
+    def check_guess(self, guess):
         try:
             number = int(guess)
         except:
@@ -81,4 +110,5 @@ class GuessNumber:
 
 
 game = GuessNumber()
+
 game.play()
